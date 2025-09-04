a78e53b6c30523c81e97985b5788956ad93b6024
Mahfuzbgh <mahfuzbgh2025@gmail.com>
2025-09-04 22:27:11 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("bazar-cache").then(cache => {
+    caches.open("bazarlist-cache").then(cache => {
       return cache.addAll([
-        "./",
         "index.html",
         "style.css",
         "app.js",
-        "logo.png"
+        "manifest.json",
+        "offline.html"
       ]);
     })
   );
@@ -14,6 +14,8 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request).catch(() => caches.match("offline.html"));
+    })
   );
 });
