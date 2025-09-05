76fabcf4035bb894dd31de7daf775aa8f994e95c
Mahfuzbgh <mahfuzbgh2025@gmail.com>
2025-09-05 11:53:02 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 const CACHE_NAME = "bazarlist-cache-v1";
 const urlsToCache = [
-  "/BazarList-PWA/",
-  "/BazarList-PWA/index.html",
-  "/BazarList-PWA/style.css",
-  "/BazarList-PWA/app.js",
-  "/BazarList-PWA/manifest.json",
-  "/BazarList-PWA/android-launchericon-192-192.png",
-  "/BazarList-PWA/android-launchericon-512-512.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./android-launchericon-192-192.png",
+  "./android-launchericon-512-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
