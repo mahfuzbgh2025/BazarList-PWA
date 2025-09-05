bb973478fc29c1d004d486288c9a59e3c1fa47fa
Mahfuzbgh <mahfuzbgh2025@gmail.com>
2025-09-05 11:49:09 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-self.addEventListener("install", (e) => {
+const CACHE_NAME = "bazarlist-cache-v1";
+const urlsToCache = [
+  "/BazarList-PWA/",
+  "/BazarList-PWA/index.html",
+  "/BazarList-PWA/style.css",
+  "/BazarList-PWA/app.js",
+  "/BazarList-PWA/manifest.json",
+  "/BazarList-PWA/android-launchericon-192-192.png",
+  "/BazarList-PWA/android-launchericon-512-512.png"
+];
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("bazarlist-cache").then((cache) => {
-      return cache.addAll([
-        "/BazarList-PWA/",
-        "/BazarList-PWA/index.html",
-        "/BazarList-PWA/style.css",
-        "/BazarList-PWA/app.js",
-        "/BazarList-PWA/manifest.json",
-        "/BazarList-PWA/android-launchericon-192-192.png",
-        "/BazarList-PWA/android-launchericon-512-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
